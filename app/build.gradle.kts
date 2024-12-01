2a59678447aca7e3553d1be4ac75ccaa4e706ec0
sohan-beniwal <sohanbeniwal97283@gmail.com>
2024-12-01 18:12:35 +0530

    Constraint Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
